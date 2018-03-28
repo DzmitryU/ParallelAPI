b35a066797cd21970c7cfd165f1347658fcffc01
dzmitryu <dimafromgrodno@yandex.ru>
2018-03-28 09:47:02 +0300

    Added promisification

--- index.js
+++ index.js
@@ -1,5 +1,38 @@
-const makeJobs = (jobs, parallelJobsNumber) => {
+const splitArray = (array, chunkSize) => {
+    const chunks = [];
 
+    for (let i = 0; i < array.length; i += chunkSize) {
+        chunks.push(array.slice(i, i + chunkSize))
+    }
+
+    return chunks;
+};
+
+const promisifyJobs = (jobs) => {
+    const promisifiedJobs = [];
+
+    jobs.forEach((job) => {
+        promisifiedJobs.push(
+            new Promise((resolve) => {
+                job(resolve);
+            })
+        )
+    });
+
+    return promisifiedJobs;
+};
+
+const makeJobs = (jobs, parallelJobsNumber) => {
+    const jobChunks = splitArray(jobs, parallelJobsNumber);
+
+    let promiseChain = Promise.all([Promise.resolve()]);
+
+    jobChunks.forEach((chunk) => {
+        const promisifiedJobs = promisifyJobs(chunk);
+        promiseChain = promiseChain.then(Promise.all(promisifiedJobs));
+    });
+
+    return promiseChain;
 };
 
 class Parallel {
